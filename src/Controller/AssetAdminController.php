ff554c671d1e850c71941068865f01edadef8256
Chris Ben <chris.ben9@gmail.com>
2020-08-03 02:15:05 +0200

    fix bug, that opened asset manager on every drop

--- src/Controller/AssetAdminController.php
+++ src/Controller/AssetAdminController.php
@@ -54,7 +54,9 @@ final class AssetAdminController extends AdminController
         // this should already have happend through the middleware
         $this->checkCsrfToken($request);
         $file = $request->files->get('grapes-upload');
-        assert($file instanceof UploadedFile);
+        if (!$file instanceof UploadedFile) {
+            return $this->json([]);
+        }
         // todo: register as service
         $config = \Pimcore::getContainer()->get(Config::class);
         $defaultUploadPath = $config['assets']['default_upload_path'] ?? '/';
@@ -66,11 +68,11 @@ final class AssetAdminController extends AdminController
         $parent = Asset::getByPath('/'. $defaultUploadPath);
         assert($parent instanceof Asset\Folder);
         $asset = Asset::getByPath('/'. $defaultUploadPath . '/' . $filename) ?? Asset::create($parent->getId(), [
-            'filename' => $filename,
-            'sourcePath' => $file->getRealPath(),
-            'userOwner' => $this->getAdminUser()->getId(),
-            'userModification' => $this->getAdminUser()->getId()
-        ]);
+                'filename' => $filename,
+                'sourcePath' => $file->getRealPath(),
+                'userOwner' => $this->getAdminUser()->getId(),
+                'userModification' => $this->getAdminUser()->getId()
+            ]);
         @unlink($file->getPath());
         $thumbnail = $asset->getThumbnail('pimcore-asset');
 
